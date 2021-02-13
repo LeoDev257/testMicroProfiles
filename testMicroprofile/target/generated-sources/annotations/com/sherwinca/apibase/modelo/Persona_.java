9048bc6698719472c98f17fde43f3676f91a4135
jpurquilla <33208867+jpurquilla@users.noreply.github.com>
2021-02-13 13:57:31 -0600

    Add files via upload

---
+++ testMicroprofile/target/generated-sources/annotations/com/sherwinca/apibase/modelo/Persona_.java
@@ -0,0 +1,16 @@
+package com.sherwinca.apibase.modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-13T12:54:31")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, Integer> idpersona;
+    public static volatile SingularAttribute<Persona, String> apellido;
+    public static volatile SingularAttribute<Persona, String> genero;
+    public static volatile SingularAttribute<Persona, String> nombre;
+
+}
